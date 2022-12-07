8073c8bd0c0e8e57f94819b6262f9571a127ff26
Jay <95107672+Jay-zar@users.noreply.github.com>
2022-12-06 21:52:56 -0500

    Added controller and routes for comments, moved comment html to a partial.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
  # resources :pages
+ #the above line makes routes for a resource automatically that match the following routes
   get '/pages', to: 'pages#index'
   get '/pages/new', to: 'pages#new', as: 'new_page'
   get '/pages/:id', to: 'pages#show', as: 'page'
@@ -8,5 +8,18 @@ Rails.application.routes.draw do
   get 'pages/:id/edit', to: 'pages#edit', as: 'edit_page'
   patch '/pages/:id', to: 'pages#update'
   delete '/pages/:id', to: 'pages#destroy'
+  resources :posts do
+  #comments routing
+  #Nesting this in a block under the :posts routes adds the posts prefix to comments route name
+  #Likewise, these can replaced with "resources :comments" to the same effect
+    get    '/comments',          to: 'comments#index', as: 'comments'
+    post   '/comments',          to: 'comments#create'
+    get    '/comments/new',      to: 'comments#new',   as: 'new_comment'
+    get    '/comments/:id',      to: 'comments#show',  as: 'comment'
+    get    '/comments/:id/edit', to: 'comments#edit',  as: 'edit_comment'
+    patch  '/comments/:id',      to: 'comments#update'
+    delete '/comments/:id',      to: 'comments#destroy'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
